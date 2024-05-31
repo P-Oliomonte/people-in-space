e244620362f47178d57b97c2000e85691584d482
Phillip Oehlenberg <hi@phillip-oehlenberg.com>
2024-05-31 14:55:24 +0200

    Update people-in-space app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
+  const [peopleInSpace, setPeopleInSpace] = React.useState(null);
+
+  async function getPeopleInSpace() {
+    const response = await fetch("http://api.open-notify.org/astros.json");
+    const dataOfPeopleInSpace = await response.json();
+
+    return dataOfPeopleInSpace;
+  }
+
+  React.useEffect(() => {
+    async function fetchData() {
+      const data = await getPeopleInSpace();
+      setPeopleInSpace(data);
+    }
+    fetchData();
+  }, []);
+
+  console.log(peopleInSpace);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <main className="main-container">
+        <div className="content-container">
+          <h1 className="headline">PEOPLE IN SPACE</h1>
+          <h2 className="subheadline">
+            The current number of people in space is{" "}
+          </h2>
+          <h2 className="number">
+            {peopleInSpace ? peopleInSpace.number : "...loading"}
+          </h2>
+          <ul className="list">
+            {peopleInSpace
+              ? peopleInSpace.people.map((person) => (
+                  <li className="list-item" key={person.name}>
+                    <strong>{person.name}</strong> is currently in space on{" "}
+                    <strong>{person.craft}</strong>
+                  </li>
+                ))
+              : "...loading"}
+          </ul>
+        </div>
+      </main>
     </div>
   );
 }
